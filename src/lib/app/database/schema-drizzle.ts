b38331077e5f19244461b366fd59468fdfae8f4c
lutfiangga <lutfiangga2908@gmail.com>
2025-12-12 00:30:39 +0700

    remove redundan db

--- src/lib/app/database/schema-drizzle.ts
+++
@@ -1,4 +0,0 @@
-export * from '../modules/user/models/user.schema';
-export * from '../modules/product/models/product.schema';
-export * from '../modules/category/models/category.schema';
-export * from '../modules/outlet/models/outlet.schema';
